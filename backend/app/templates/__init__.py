90cdb712abb1894572b6ca32ccf5eddb92a4faba
Tom Hartman <thomas.lees.hartman@gmail.com>
2020-08-27 18:09:12 -0400

    Adding LIC and backend templates
    Adding in the backend templates for adding recipes and a general LICENSE

---
+++ backend/app/templates/__init__.py
@@ -0,0 +1,25 @@
+###############################################################################
+## __init__.py for sous-chef backend                                          ##
+## Copyright (c) 2020 Tom Hartman (thomas.lees.hartman@gmail.com)            ##
+##                                                                           ##
+## This program is free software; you can redistribute it and/or             ##
+## modify it under the terms of the GNU General Public License               ##
+## as published by the Free Software Foundation; either version 2            ##
+## of the License, or the License, or (at your option) any later             ##
+## version.                                                                  ##
+##                                                                           ##
+## This program is distributed in the hope that it will be useful,           ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of            ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             ##
+## GNU General Public License for more details.                              ##
+###############################################################################
+
+### Commentary ## {{{
+##
+## 
+##
+## }}}
+
+### __init__ ## {{{
+
+## }}}
